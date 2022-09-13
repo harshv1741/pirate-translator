0f0e4a324f3338c871b33efe71f24906d4990f3c
harshv1741 <ardostree2001@outlook.com>
2022-09-13 22:23:39 +0530

    Designing the minion app

---
+++ app.js
@@ -0,0 +1,9 @@
+// const message = document.getElementById("message");
+
+const message = document.getElementById('message');
+const btnTranslate = document.getElementById('translate');
+
+// ✅ GET value of textarea
+console.log(message.value); // 👉️ ""
+
+// ✅ get value of textarea on change
